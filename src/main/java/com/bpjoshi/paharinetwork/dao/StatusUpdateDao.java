07d8b97ef04a2f744b000bcc3054ccdf17f398e3
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-28 00:44:31 +0530

    Method added to find latest Status

--- src/main/java/com/bpjoshi/paharinetwork/dao/StatusUpdateDao.java
+++ src/main/java/com/bpjoshi/paharinetwork/dao/StatusUpdateDao.java
@@ -13,5 +13,7 @@ import com.bpjoshi.paharinetwork.model.StatusUpdate;;
  * Type of id should be object type, it doesn't accept premitive types
  */
 public interface StatusUpdateDao extends CrudRepository<StatusUpdate, Long> {
-
+	//Added to retrieve latest status update.
+	//Spring data jpa adds implementation ..based upon our object properties
+	StatusUpdate findFirstByOrderByStatusDateDesc();
 }
