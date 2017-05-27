8e65fbedfd0ecd3a3f8a8f27458fe597bbfce075
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-27 17:46:37 +0530

    StatusUpdateDao added which extends CrudRepository interface

---
+++ src/main/java/com/bpjoshi/paharinetwork/dao/StatusUpdateDao.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.bpjoshi.paharinetwork.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bpjoshi.paharinetwork.model.StatusUpdate;;
+
+/**
+ * @author Bhagwati Prasad(bpjoshi)
+ * StatusUpdate represents model StatusUpdate and Long represent type of statusId
+ * Type of id should be object type, it doesn't accept premitive types
+ */
+public interface StatusUpdateDao extends CrudRepository<StatusUpdate, Long> {
+
+}
