1bff1028e01f34efab5a936d1e9255e596df7ea5
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-30 23:59:27 +0530

    Return status on page basis

--- src/main/java/com/bpjoshi/paharinetwork/service/StatusUpdateService.java
+++ src/main/java/com/bpjoshi/paharinetwork/service/StatusUpdateService.java
@@ -4,6 +4,9 @@
 package com.bpjoshi.paharinetwork.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.bpjoshi.paharinetwork.dao.StatusUpdateDao;
@@ -17,13 +20,22 @@ import com.bpjoshi.paharinetwork.model.StatusUpdate;
 public class StatusUpdateService {
 	@Autowired
 	private StatusUpdateDao statusUpdateDao;
-	
+	private final static int PAGESIZE=3;
 	public void saveStatusUpdate(StatusUpdate statusUpdate){
 		statusUpdateDao.save(statusUpdate);
 	}
 	
 	public StatusUpdate getLatestStatusUpdate(){
 		return statusUpdateDao.findFirstByOrderByStatusDateDesc();
+	}
+	
+	/*
+	 * To retrieve number of statuses in a page on DESC order of statusDate
+	 * PageSize is 3 status updates per page
+	 */
+	public Page<StatusUpdate> getPage(int pageNumber){
+		PageRequest pageRequest=new PageRequest(pageNumber-1, PAGESIZE, Sort.Direction.DESC, "statusDate");
+		return statusUpdateDao.findAll(pageRequest);
 	}
 
 }
