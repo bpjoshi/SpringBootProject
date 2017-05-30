d5119a6a0565783dcbca75f9cdcaf4bc04a3749a
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-30 23:59:04 +0530

    PagingAndSorting

--- src/main/java/com/bpjoshi/paharinetwork/dao/StatusUpdateDao.java
+++ src/main/java/com/bpjoshi/paharinetwork/dao/StatusUpdateDao.java
@@ -3,16 +3,16 @@
  */
 package com.bpjoshi.paharinetwork.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.bpjoshi.paharinetwork.model.StatusUpdate;;
+import com.bpjoshi.paharinetwork.model.StatusUpdate;
 
 /**
  * @author Bhagwati Prasad(bpjoshi)
  * StatusUpdate represents model StatusUpdate and Long represent type of statusId
  * Type of id should be object type, it doesn't accept premitive types
  */
-public interface StatusUpdateDao extends CrudRepository<StatusUpdate, Long> {
+public interface StatusUpdateDao extends PagingAndSortingRepository<StatusUpdate, Long> {
 	//Added to retrieve latest status update.
 	//Spring data jpa adds implementation ..based upon our object properties
 	StatusUpdate findFirstByOrderByStatusDateDesc();
