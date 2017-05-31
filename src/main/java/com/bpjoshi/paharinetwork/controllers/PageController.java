224e3f8a5a1ba33c50dd887ba7d052e50141201b
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-01 01:51:14 +0530

    Home Page changes

--- src/main/java/com/bpjoshi/paharinetwork/controllers/PageController.java
+++ src/main/java/com/bpjoshi/paharinetwork/controllers/PageController.java
@@ -1,7 +1,12 @@
 package com.bpjoshi.paharinetwork.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bpjoshi.paharinetwork.model.StatusUpdate;
+import com.bpjoshi.paharinetwork.service.StatusUpdateService;
 /**
  * 
  * @author Bhagwati Prasad(bpjoshi)
@@ -9,10 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class PageController {
+	@Autowired
+	private StatusUpdateService statusUpdateService;
 	@RequestMapping("/")
-    String home() {
-		//changed to app.homepage from home to display from tiles
-        return "app.homepage";
+    ModelAndView home(ModelAndView modelAndView) {
+		StatusUpdate statusUpdate=statusUpdateService.getLatestStatusUpdate();
+		
+		modelAndView.getModel().put("statusUpdate", statusUpdate);
+		modelAndView.setViewName("app.homepage");
+        return modelAndView;
     }
 	
 	@RequestMapping("/about")
--- src/main/webapp/WEB-INF/tiles/home.jsp
+++ src/main/webapp/WEB-INF/tiles/home.jsp
@@ -1,3 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
-C'est l'home page.
+Welcome to Pahari Network!
+<div class="row">
+	<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+		<div class="homepage-status">
+			${statusUpdate.statusText}
+		</div>
+	</div>
+</div>
