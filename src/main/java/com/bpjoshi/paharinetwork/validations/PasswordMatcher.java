4fdba18366e3b372f7073a53ce572335a9fd7a0c
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-07 22:55:53 +0530

    PasswordMatcher Annotation body

--- src/main/java/com/bpjoshi/paharinetwork/validations/PasswordMatcher.java
+++ src/main/java/com/bpjoshi/paharinetwork/validations/PasswordMatcher.java
@@ -3,10 +3,27 @@
  */
 package com.bpjoshi.paharinetwork.validations;
 
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Payload;
+
 /**
  * @author Bhagwati Prasad(bpjoshi)
  *
  */
+@Target({TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented //helps to see values when you hover over
 public @interface PasswordMatcher {
-
+	
+	String message() default "{error.password.mismatch}";
+	
+	Class<?>[] group() default{};
+	
+	Class <? extends Payload>[] payload() default {};
 }
