6710aeda43858af70c8c30ba6916bb6d5c1ae742
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-08 00:12:09 +0530

    added groups() which closes #14

--- src/main/java/com/bpjoshi/paharinetwork/validations/PasswordMatcher.java
+++ src/main/java/com/bpjoshi/paharinetwork/validations/PasswordMatcher.java
@@ -4,10 +4,10 @@
 package com.bpjoshi.paharinetwork.validations;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
@@ -17,15 +17,12 @@ import javax.validation.Payload;
  * @author Bhagwati Prasad(bpjoshi)
  *
  */
-@Target({TYPE})
-@Retention(RetentionPolicy.RUNTIME)
+@Target(TYPE)
+@Retention(RUNTIME)
 @Constraint(validatedBy=PasswordMatchValidator.class)
 @Documented //helps to see values when you hover over
 public @interface PasswordMatcher {
-	
 	String message() default "{error.password.mismatch}";
-	
-	Class<?>[] group() default{};
-	
-	Class <? extends Payload>[] payload() default {};
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
 }
