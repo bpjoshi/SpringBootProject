9c11be3d85417b19ecd4b047afe71885b8735b2e
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-10 21:47:36 +0530

    Renamed Enum

---
+++ src/main/java/com/bpjoshi/paharinetwork/model/VerificationTokenType.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.bpjoshi.paharinetwork.model;
+
+/**
+ * @author Bhagwati Prasad(bpjoshi)
+ *
+ */
+public enum VerificationTokenType {
+	REGISTRATION,
+	PASSWORD_RESET
+}
