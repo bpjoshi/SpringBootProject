bdc54b18c9edc5b23d23fbe360aeab4c4bb3b6f8
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-10 22:34:53 +0530

    Dao Class for verfication added

---
+++ src/main/java/com/bpjoshi/paharinetwork/dao/VerificationTokenDao.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.bpjoshi.paharinetwork.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bpjoshi.paharinetwork.model.VerificationToken;
+
+/**
+ * @author Bhagwati Prasad(bpjoshi)
+ */
+@Repository
+public interface VerificationTokenDao extends CrudRepository<VerificationToken, Long> {
+	VerificationToken findByTokenString(String tokenString);
+}
