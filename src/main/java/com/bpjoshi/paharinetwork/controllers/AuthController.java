744f0f9d4841db5ad40a8d1e9ae5952a3829fa6f
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-11 13:46:01 +0530

    handle token verification

--- src/main/java/com/bpjoshi/paharinetwork/controllers/AuthController.java
+++ src/main/java/com/bpjoshi/paharinetwork/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package com.bpjoshi.paharinetwork.controllers;
 
+import java.util.Date;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bpjoshi.paharinetwork.model.EndUser;
+import com.bpjoshi.paharinetwork.model.VerificationToken;
 import com.bpjoshi.paharinetwork.service.EndUserService;
 import com.bpjoshi.paharinetwork.service.MailService;
+import com.bpjoshi.paharinetwork.service.VerificationTokenService;
 /**
  * 
  * @author Bhagwati Prasad(bpjoshi)
@@ -30,6 +35,8 @@ public class AuthController {
 	private String tokenInvalid;
 	@Value("${message.token.expired}")
 	private String tokenExpired;
+	@Autowired
+	private VerificationTokenService verificationTokenService;
 	@RequestMapping("/login")
     String adminPage() {
         return "app.login";
@@ -49,7 +56,8 @@ public class AuthController {
 		modelAndView.setViewName("app.register");
 		if(!result.hasErrors()){
 			endUserService.registerEndUser(endUser);
-			emailService.sendVerificationEmail(endUser.getUserEmail());
+			String verificationToken=verificationTokenService.createEmailVerificationToken(endUser);
+			emailService.sendVerificationEmail(endUser.getUserEmail(), verificationToken);
 			modelAndView.setViewName("redirect:/verifyemail");
 		}
 		return modelAndView;
@@ -63,8 +71,25 @@ public class AuthController {
 	 * If user clicks on a valid token, account is confirmed.
 	 */
 	@RequestMapping(value="/regconfirmed")
-    ModelAndView tokenRegConfirmed(ModelAndView modelAndView) {
-		
+    ModelAndView tokenRegConfirmed(ModelAndView modelAndView, @RequestParam("t") String vToken) {
+		VerificationToken verificationToken=verificationTokenService.getVerificationToken(vToken);
+		//Check if the token doesn't exist in database
+		if(verificationToken==null){
+			modelAndView.setViewName("redirect:/invaliduser");
+			return modelAndView;
+		}
+		//Check if the token is expired
+		if(verificationToken.getExpiryDate().before(new Date())){
+			modelAndView.setViewName("redirect:/expiredlink");
+			return modelAndView;
+		}
+		//Check if a user exist for this token
+		if(verificationToken.getEndUser()==null){
+			modelAndView.setViewName("redirect:/invaliduser");
+			return modelAndView;
+		}
+		verificationToken.getEndUser().setUserEnabled(true);
+		endUserService.save(verificationToken.getEndUser());
 		modelAndView.getModel().put("message", tokenRegConfirmed);
 		modelAndView.setViewName("app.message");
 		return modelAndView;
