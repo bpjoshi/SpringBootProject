e58f6f0333f11b2a7ac1fd241086f7c18ee27edc
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-11 13:46:38 +0530

    links updated with permission

--- src/main/java/com/bpjoshi/paharinetwork/security/WebSecurityConfig.java
+++ src/main/java/com/bpjoshi/paharinetwork/security/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		//http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
 		//Allow css etc at home page
 		http.authorizeRequests().antMatchers("/", "/about", "/register", 
-				"/verifyemail", "/registrationconfirmed", "/expiredlink", 
+				"/verifyemail", "/regconfirmed", "/expiredlink", 
 				"/invaliduser").permitAll().antMatchers("/js/*", "/css/*", "/img/*")
 		.permitAll().antMatchers("/viewstatus", "/addstatus", "/deletestatus", 
 				"/editstatus").hasRole("ADMIN").and().formLogin().loginPage("/login")
