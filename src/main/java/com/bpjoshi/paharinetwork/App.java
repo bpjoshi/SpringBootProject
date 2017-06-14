09a075b443c129dcd258c04f7cd9d2979468269a
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-15 03:24:59 +0530

    PolicyFactory bean from OWASP generating here

--- src/main/java/com/bpjoshi/paharinetwork/App.java
+++ src/main/java/com/bpjoshi/paharinetwork/App.java
@@ -1,15 +1,13 @@
 package com.bpjoshi.paharinetwork;
 
 
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -56,5 +54,13 @@ public class App extends SpringBootServletInitializer{
 	@Bean
 	PasswordEncoder getPasswordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+	/*
+	 * HTML sanitizer bean of owasp-java-html-sanitizer
+	 */
+	@Bean
+	PolicyFactory getUserHtmlPolicy(){
+		return new HtmlPolicyBuilder().allowCommonBlockElements()
+				.allowCommonInlineFormattingElements().toFactory();
 	}
 }
