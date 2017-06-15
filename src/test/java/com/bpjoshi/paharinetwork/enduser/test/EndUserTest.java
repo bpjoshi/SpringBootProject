685af23207488ffb811a12a89256c1967e6af7c7
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-16 04:20:30 +0530

    EndUserTest

--- src/test/java/com/bpjoshi/paharinetwork/enduser/test/EndUserTest.java
+++ src/test/java/com/bpjoshi/paharinetwork/enduser/test/EndUserTest.java
@@ -28,22 +28,48 @@ import com.bpjoshi.paharinetwork.service.EndUserService;
 public class EndUserTest {
 	@Autowired
 	private EndUserService endUserService;
+	/*
+	 * This method tests registerEndUser() method of EndUserService
+	 */
 	@Test
 	public void registerEndUserTest(){
-		EndUser endUser=createEndUserObjectForTest("hello@hello.com");
+		EndUser endUser=createEndUserObjectForTest("bonjour@france.fr");
 		endUserService.registerEndUser(endUser);
-		assertNotNull("User Id should not be null", endUser.getUserId());
-		assertFalse("User shouldn't be enabled", endUser.getUserEnabled());
-		assertEquals("A new user's role should be 'USER' ", "ROLE_USER", endUser.getUserRole());
+		assertNotNull("User Id should not be null after registration", endUser.getUserId());
+		assertFalse("User shouldn't be enabled till he clicks on verfication link on email", 
+				endUser.getUserEnabled());
+		assertEquals("A newly registered user's role should be 'ROLE_USER' ", "ROLE_USER", 
+				endUser.getUserRole());
 	}
+	/*
+	 * This method tests saveEndUser() method of EndUserService. The saveEndUser() method is
+	 * called after updating the role. It should reflect the updated role in test.
+	 */
+	@Test
+	public void saveEndUserTest(){
+		EndUser endUser=createEndUserObjectForTest("czesc@polska.pl");
+		endUserService.registerEndUser(endUser);
+		assertEquals("A newly registered user's role should be 'ROLE_USER' ", "ROLE_USER",
+				endUser.getUserRole());
+		endUser.setUserRole("ROLE_ADMIN");
+		endUserService.saveEndUser(endUser);
+		assertEquals("After saveEndUser() call role should be that of Admin ", "ROLE_ADMIN", 
+				endUser.getUserRole());
+	}
+	/*
+	 * This methods tests getEndUser() method of EndUserService
+	 * The registered User and the retrieved EndUser object using getEndUser() 
+	 * method of EndUserService must be the same
+	 */
 	@Test
 	public void findByUserEmailTest(){
-		EndUser endUser=createEndUserObjectForTest("abc@def.com");
-		endUserService.save(endUser);
-		EndUser newEndUser=endUserService.getEndUser("abc@def.com");
-		assertEquals("The emailid must be same", "abc@def.com", newEndUser.getUserEmail());
+		EndUser endUser=createEndUserObjectForTest("namaste@india.in");
+		endUserService.registerEndUser(endUser);
+		assertEquals("The two EndUser object must be Equal", endUser, endUserService.getEndUser("namaste@india.in"));
 	}
-	
+	/*
+	 * This method is used just to create objects of EndUser for the purpose of testing
+	 */
 	public static EndUser createEndUserObjectForTest(String email){
 		EndUser endUser=new EndUser();
 		endUser.setUserEmail(email);
