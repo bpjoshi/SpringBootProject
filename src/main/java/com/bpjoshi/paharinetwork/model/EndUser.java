bc335e07e654c26306ab04583d34af01f7d3cf92
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-16 04:19:46 +0530

    equals and hashcode method added

--- src/main/java/com/bpjoshi/paharinetwork/model/EndUser.java
+++ src/main/java/com/bpjoshi/paharinetwork/model/EndUser.java
@@ -88,5 +88,43 @@ public Boolean getUserEnabled() {
 public void setUserEnabled(Boolean userEnabled) {
 	this.userEnabled = userEnabled;
 }
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((userEmail == null) ? 0 : userEmail.hashCode());
+	result = prime * result + (userEnabled ? 1231 : 1237);
+	result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+	result = prime * result + ((userRole == null) ? 0 : userRole.hashCode());
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	EndUser other = (EndUser) obj;
+	if (userEmail == null) {
+		if (other.userEmail != null)
+			return false;
+	} else if (!userEmail.equals(other.userEmail))
+		return false;
+	if (userEnabled != other.userEnabled)
+		return false;
+	if (userId == null) {
+		if (other.userId != null)
+			return false;
+	} else if (!userId.equals(other.userId))
+		return false;
+	if (userRole == null) {
+		if (other.userRole != null)
+			return false;
+	} else if (!userRole.equals(other.userRole))
+		return false;
+	return true;
+}
 
 }
