4f8755906e1d570e8d23c67436f524cee614e6cc
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-18 02:38:59 +0530

    Method to create or select a directory so that pics can be put there

--- src/main/java/com/bpjoshi/paharinetwork/service/MediaService.java
+++ src/main/java/com/bpjoshi/paharinetwork/service/MediaService.java
@@ -1,5 +1,8 @@
 package com.bpjoshi.paharinetwork.service;
 
+import java.io.File;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ public class MediaService {
 	private String validImageExtensions;
 	@Value("${media.video.extensions}")
 	private String validVideoExtensions;
+	private Random randomNumber=new Random();
 	
 	private String getMediaExtension(String fileName){
 		int positionOfDot=fileName.lastIndexOf(".");
@@ -38,5 +42,15 @@ public class MediaService {
 			}
 		}
 		return false;
+	}
+	
+	private File selectMediaDirectory(String rootPath, String prefix){
+		int numDir=randomNumber.nextInt(1000);
+		String newSubDir=String.format("%s%03d", prefix, numDir);
+		File newDir=new File(rootPath, newSubDir);
+		if(!newDir.exists()){
+			newDir.mkdir();
+		}
+		return newDir;
 	}
 }
