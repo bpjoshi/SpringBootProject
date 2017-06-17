592c369b3d1366a05b8d7d00114d42e257815960
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-17 23:02:35 +0530

    Valid Image and Valid Video Extension

--- src/main/java/com/bpjoshi/paharinetwork/service/MediaService.java
+++ src/main/java/com/bpjoshi/paharinetwork/service/MediaService.java
@@ -1,5 +1,6 @@
 package com.bpjoshi.paharinetwork.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,6 +9,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MediaService {
+	@Value("${media.image.extensions}")
+	private String validImageExtensions;
+	@Value("${media.video.extensions}")
+	private String validVideoExtensions;
 	
 	private String getMediaExtension(String fileName){
 		int positionOfDot=fileName.lastIndexOf(".");
