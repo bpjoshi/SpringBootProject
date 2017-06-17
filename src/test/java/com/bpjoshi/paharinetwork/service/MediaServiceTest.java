9bcd7db84c899c2d6370545079325295316835ff
bpjoshi <bpjoshi@bpjoshi.com>
2017-06-18 00:55:09 +0530

    Updated tests for image and video extension validation

--- src/test/java/com/bpjoshi/paharinetwork/service/MediaServiceTest.java
+++ src/test/java/com/bpjoshi/paharinetwork/service/MediaServiceTest.java
@@ -19,10 +19,36 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class MediaServiceTest {
 	@Autowired
 	private MediaService mediaService;
+	/*
+	 * Test method to get extension
+	 */
 	@Test
 	public void getMediaExtensionTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
 		Method method=MediaService.class.getDeclaredMethod("getMediaExtension", String.class);
 		method.setAccessible(true);
 		assertEquals("Should be a JPEG", "jpeg", (String)method.invoke(mediaService, "myPhot.jpeg"));
+		assertEquals("Should be a JPEG", "xlsx", (String)method.invoke(mediaService, "myPhot.xlsx"));
+		assertNull("Should be a JPEG", (String)method.invoke(mediaService, "myPhot"));
 	}
+	/*
+	 * Test method for isAnImageExtension method
+	 */
+	@Test
+	public void isAnImageExtensionTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method=MediaService.class.getDeclaredMethod("isAnImageExtension", String.class);
+		method.setAccessible(true);
+		//assertTrue("Should fail", (Boolean)method.invoke(mediaService, "myPhot"));
+		assertTrue("Should pass", (Boolean)method.invoke(mediaService, "jpeg"));
+	}
+	/*
+	 * Test method for isAVideoExtension method
+	 */
+	@Test
+	public void isAVideoExtensionTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method method=MediaService.class.getDeclaredMethod("isAVideoExtension", String.class);
+		method.setAccessible(true);
+		//assertTrue("Should fail", (Boolean)method.invoke(mediaService, "myPhot"));
+		assertTrue("Should pass", (Boolean)method.invoke(mediaService, "mp4"));
+	}
+	
 }
